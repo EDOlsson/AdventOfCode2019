5c8e921a1a44b1fb89d37fc4f68ef1b08bbb4dd0
Eric Olsson <eric.olsson@docusign.com>
2019-12-04 15:32:16 -0600

    Improve Day2-Part1 implementation

--- AdventOfCode/Day02.fs
+++ AdventOfCode/Day02.fs
@@ -2,82 +2,90 @@ module Day2
 
 open Test
 
-type Instruction =
-    | Add
-    | Multiply
+type OpCode =
+    | Add of int * int * int
+    | Multiply of int * int * int
     | Halt
+
+type Computer = {
+    memory : int[]
+    instructionPointer : int
+}
 
 let readInput =
     let rawInput = System.IO.File.ReadAllText "day-2-input"
     rawInput.Split([|','|])
     |> Array.map System.Convert.ToInt32
 
-let private translateInstruction opCode =
-    match opCode with
-    | 1 -> Add
-    | 2 -> Multiply
+let private translateInstruction computer =
+    let readMemoryOffset computer offset =
+        computer.memory.[computer.instructionPointer + offset]
+
+    match readMemoryOffset computer 0 with
+    | 1 -> Add (readMemoryOffset computer 1, readMemoryOffset computer 2, readMemoryOffset computer 3)
+    | 2 -> Multiply (readMemoryOffset computer 1, readMemoryOffset computer 2, readMemoryOffset computer 3)
     | 99 -> Halt
-    | _ -> failwith (sprintf "Bad instruction %d" opCode)
+    | _ -> failwith (sprintf "Bad instruction %d" (readMemoryOffset computer 0))
 
-let private executeInstruction memory operands instruction =
-    let add' (memory : int[]) (operands : int[]) =
-        let addend1 = memory.[operands.[1]]
-        let addend2 = memory.[operands.[2]]
-        Array.set memory operands.[3] (addend1 + addend2)
+let rec private executeInstruction computer =
+    let executeBinaryInstruction computer operation param1 param2 destination =
+        let p1 = computer.memory.[param1]
+        let p2 = computer.memory.[param2]
+        let result = operation p1 p2
+        let memory' = Array.copy computer.memory
+        Array.set memory' destination result
+        { computer with memory = memory'; instructionPointer = computer.instructionPointer + 4 }
 
-    let multiply' (memory : int[]) (operands : int[]) =
-        let multiplier = memory.[operands.[1]]
-        let multiplicand = memory.[operands.[2]]
-        Array.set memory operands.[3] (multiplier * multiplicand)
+    let instruction = translateInstruction computer
 
     match instruction with
-    | Halt -> None
-    | Add ->
-        add' memory operands
-        Some 1
-    | Multiply ->
-        multiply' memory operands
-        Some 1
+    | Halt -> computer
+    | Add (param1, param2, destination) ->
+        let result = executeBinaryInstruction computer (+) param1 param2 destination
+        executeInstruction result
+
+    | Multiply (param1, param2, destination) ->
+        let result = executeBinaryInstruction computer (*) param1 param2 destination
+        executeInstruction result
 
 let executeProgram (memory : int[]) =
-    let mutable result = Some 42
-    let mutable strideIndex = 0
-    while result.IsSome do
-        let opCode = memory.[4 * strideIndex]
-        let instruction = translateInstruction opCode
+    let computer = executeInstruction { memory = memory; instructionPointer = 0 }
 
-        let operands = memory.[4 * strideIndex..]
-
-        result <- executeInstruction memory operands instruction
-
-        strideIndex <- strideIndex + 1
+    computer.memory
 
 let testDay2Part1 =
     let input1 = [|1; 0; 0; 0; 99|]
-    executeProgram input1
+    let result1 = executeProgram input1
 
-    input1 |> shouldEqual [|2; 0; 0; 0; 99|]
+    result1 |> shouldEqual [|2; 0; 0; 0; 99|]
 
     let input2 = [|2; 3; 0; 3; 99|]
-    executeProgram input2
+    let result2 = executeProgram input2
 
-    input2 |> shouldEqual [|2; 3; 0; 6; 99|]
+    result2 |> shouldEqual [|2; 3; 0; 6; 99|]
 
     let input3 = [|2; 4; 4; 5; 99; 0|]
-    executeProgram input3
+    let result3 = executeProgram input3
 
-    input3 |> shouldEqual [|2; 4; 4; 5; 99; 9801|]
+    result3 |> shouldEqual [|2; 4; 4; 5; 99; 9801|]
 
     let input4 = [|1; 1; 1; 4; 99; 5; 6; 0; 99|]
-    executeProgram input4
+    let result4 = executeProgram input4
 
-    input4 |> shouldEqual [|30; 1; 1; 4; 2; 5; 6; 0; 99|]
+    result4 |> shouldEqual [|30; 1; 1; 4; 2; 5; 6; 0; 99|]
+
+let private initializeMemory noun verb (memory : int[]) =
+    let memory' = Array.copy memory
+
+    Array.set memory' 1 noun
+    Array.set memory' 2 verb
+
+    memory'
 
 let runDay2Part1 =
     let memory = readInput
-    Array.set memory 1 12
-    Array.set memory 2 2
+    let initializedMemory = initializeMemory 12 2 memory
 
-    executeProgram memory
+    let result = executeProgram initializedMemory
 
-    memory.[0]
+    result.[0]
