afcb94bb30dea7d3d1e5e08918cb2472dfb83b3f
Eric Olsson <eric.olsson@docusign.com>
2019-12-04 17:43:41 -0600

    Complete Day 3 part 1 (slow!)

---
+++ AdventOfCode/Day03.fs
@@ -0,0 +1,72 @@
+module Day3
+
+open Test
+
+type Point = {
+    x : int
+    y : int
+}
+
+type Direction =
+    | Up
+    | Down
+    | Left
+    | Right
+
+type WireSegment = {
+    direction : Direction
+    count : int
+}
+
+let generatePointsFromWireSegment start segment =
+    match segment.direction with
+    | Up -> [| for dy in 1..segment.count do yield { x = start.x; y = start.y + dy } |]
+    | Down -> [| for dy in 1..segment.count do yield { x = start.x; y = start.y - dy } |]
+    | Left -> [| for dx in 1..segment.count do yield { x = start.x - dx; y = start.y } |]
+    | Right -> [| for dx in 1..segment.count do yield { x = start.x + dx; y = start.y } |]
+
+let generatePointsForWirePath wirePath =
+    let folder (points, start) segment =
+        let pointsForSegment = generatePointsFromWireSegment start segment
+
+        (Array.append points pointsForSegment, Array.last pointsForSegment)
+
+    fst (Array.fold folder (Array.empty, { x = 0; y = 0 }) wirePath)
+
+let parseWirePath (input : string) =
+    let parseSegment (instruction : string) =
+        match instruction.ToUpperInvariant().[0] with
+        | 'U' -> { direction = Up; count = System.Convert.ToInt32(instruction.Substring(1)) }
+        | 'D' -> { direction = Down; count = System.Convert.ToInt32(instruction.Substring(1)) }
+        | 'L' -> { direction = Left; count = System.Convert.ToInt32(instruction.Substring(1)) }
+        | 'R' -> { direction = Right; count = System.Convert.ToInt32(instruction.Substring(1)) }
+        | _ -> failwith (sprintf "Invalid input : %s" instruction)
+
+    input.Split([|','|])
+    |> Array.map parseSegment
+
+let computeManhattanDistance point =
+    abs point.x + abs point.y
+
+let findNearestIntersection (wirePaths : string[]) =
+    let wire1 = parseWirePath wirePaths.[0] |> generatePointsForWirePath
+    let wire2 = parseWirePath wirePaths.[1] |> generatePointsForWirePath
+
+    wire1
+    |> Array.filter (fun w -> Array.contains w wire2)
+    |> Array.minBy computeManhattanDistance
+
+let testDay3Part1 =
+    let result1 = computeManhattanDistance <| findNearestIntersection [|"R8,U5,L5,D3"; "U7,R6,D4,L4"|]
+    result1 |> shouldEqual 6
+
+    let result2 = computeManhattanDistance <| findNearestIntersection [|"R75,D30,R83,U83,L12,D49,R71,U7,L72"; "U62,R66,U55,R34,D71,R55,D58,R83"|]
+    result2 |> shouldEqual 159
+
+    let result3 = computeManhattanDistance <| findNearestIntersection [|"R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"; "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"|]
+    result3 |> shouldEqual 135
+
+let day3Part1 =
+    let wirePaths = System.IO.File.ReadAllLines "day-3-input"
+
+    wirePaths |> findNearestIntersection |> computeManhattanDistance
--- AdventOfCode/Program.fs
+++ AdventOfCode/Program.fs
@@ -3,10 +3,14 @@
 open System
 open Day1
 open Day2
+open Day3
 
 [<EntryPoint>]
 let main argv =
     
+    //
+    // Day 1
+    //
     // testFuelCalculations |> ignore
 
     // let day1a = calculateTotalFuel
@@ -19,12 +23,23 @@ let main argv =
 
     // printfn "Day 1 - part 2 >> %d" day1b
 
-    testDay2Part1 |> ignore
+    //
+    // Day 2
+    //
+    // testDay2Part1 |> ignore
 
-    let day2a = runDay2Part1
-    printfn "Day 2 - part 1 >> %d" day2a
+    // let day2a = runDay2Part1
+    // printfn "Day 2 - part 1 >> %d" day2a
 
-    let day2b = runDay2Part2
-    printfn "Day 2 - part 2 >> %d" day2b
+    // let day2b = runDay2Part2
+    // printfn "Day 2 - part 2 >> %d" day2b
+
+    //
+    // Day 3
+    //
+    testDay3Part1 |> ignore
+    let day3a = day3Part1
+
+    printfn "Day 3 - part 1 >> %d" day3a
 
     0 // return an integer exit code
