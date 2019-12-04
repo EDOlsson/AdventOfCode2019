cd4ec53afd14724d3c9129de516ebb9f62105c04
Eric Olsson <eric.olsson@docusign.com>
2019-12-04 12:56:07 -0600

    Update test module to not throw

--- AdventOfCode/Test.fs
+++ AdventOfCode/Test.fs
@@ -1,4 +1,5 @@
 module Test
 
 let shouldEqual expected actual =
-    if expected <> actual then failwith (sprintf "Test case failure: Expected %A%s                     Actual %A" expected System.Environment.NewLine actual)
+    if expected <> actual then printfn "X Test case failure: Expected %A%s                       Actual %A" expected System.Environment.NewLine actual
+    else printfn ". Passed"
